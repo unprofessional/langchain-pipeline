4fbafde0e934fb896298f523d0a5c26047d7c4c9
Unprofessional <unprofessionalmadman@gmail.com>
2025-03-06 15:08:31 -0500

    Fix OpenAPI doc

--- src/openapi/operations/pipeline/pipeline.controller.js
+++ src/openapi/operations/pipeline/pipeline.controller.js
@@ -1,4 +1,7 @@
-import { runPipelineWithBufferMemory, runPipelineWithPersistence } from '../../../pipeline/pipeline.js';
+import { 
+  // runPipelineWithBufferMemory,
+  runPipelineWithPersistence,
+} from '../../../pipeline/pipeline.js';
 import initLogger from '../../../utils/winston-logger.js';
 
 const logger = initLogger();
@@ -6,18 +9,22 @@ const logger = initLogger();
 export async function pipelineControllerFn(req, res) {
   logger.info('Pipeline Controller reached');
 
-  const queryParams = req.params;
+  // const queryParams = req.params;
 
   const body = req.body;
   const userInput = body.prompt;
   const sessionId = body.sessionId || '0987654321';
+
+  logger.info('userInput: ', userInput);
+  logger.info('sessionId: ', sessionId);
+
   let response = {};
   
   try {
     // response = await runPipeline(userInput);
     // response = await runPipelineWithBufferMemory(userInput);
     response = await runPipelineWithPersistence(userInput, sessionId);
-    console.log('LLM Response[1]:', response);
+    console.log('LLM Response:', response);
   } catch (error) {
     console.error('Error in LLM pipeline:', error);
     res.status(500).end();
