31c53b870afaf1042d612f057427f6df077af0a3
Unprofessional <unprofessionalmadman@gmail.com>
2025-03-14 16:48:06 -0400

    Update SQL delete file table name

--- src/store/sql/delete_oldest_msgs_by_session_id.sql
+++ src/store/sql/delete_oldest_msgs_by_session_id.sql
@@ -1,7 +1,7 @@
-DELETE FROM messages
+DELETE FROM chat_memory
 WHERE id NOT IN (
     SELECT id 
-    FROM messages 
+    FROM chat_memory 
     WHERE session_id = $1
     ORDER BY id DESC
     LIMIT $2
